2adb094cef986b3e5f2414b7860a2f8828b1bff0
supriyadsouza <supriya.dsouza@gmail.com>
2015-03-16 20:34:08 +0100

    First commit
    TestNG, Log4j, Maven ready

---
+++ src/com/googletests/GoogleTests.java
@@ -0,0 +1,41 @@
+package com.googletests;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+//import org.testng.log4testng.Logger;
+
+public class GoogleTests {
+	
+	public WebDriver driver;
+	public static Logger log = Logger.getLogger(GoogleTests.class);
+	
+	@BeforeClass
+	public void beforeClass() {
+		BasicConfigurator.configure();
+		LogManager.getRootLogger().setLevel(Level.INFO);
+		log.info("Starting the browser");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http:\\www.google.com");
+	}
+	
+	@Test
+	public void testGoogle() {
+		log.info("In the testcase");
+	}
+  
+
+	@AfterClass
+	public void afterClass() {
+	  driver.close();
+	  driver.quit();
+	  log.info("Closing the browser");
+	}
+}
